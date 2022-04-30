94207cee7b225cb109a26a458c518c117da24199
sefatanam <sefatanam@gmail.com>
2022-05-01 00:38:46 +0600

    portfolio page technology section functionality

--- src/app/info/info.component.ts
+++ src/app/info/info.component.ts
@@ -1,10 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Technology } from '@interfaces/Technology';
 
 @Component({
   selector: 'app-info',
   templateUrl: './info.component.html',
   styleUrls: ['./info.component.scss']
 })
-export class InfoComponent {
-  constructor() {}
+export class InfoComponent implements OnInit {
+  technologies!: Observable<Technology[]>;
+
+  constructor(private httpClient: HttpClient) {}
+
+  ngOnInit(): void {
+    this.technologies = this.httpClient.get<Technology[]>('technologies.json');
+  }
 }
---
+++ src/interfaces/Technology.ts
@@ -0,0 +1,4 @@
+export interface Technology {
+  type: string;
+  list: Array<string>;
+}
