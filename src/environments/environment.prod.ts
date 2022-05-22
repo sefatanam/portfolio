f6ae1e7a23a3fc61af83887e481a2fdcff05847b
thesefat <sefat@metadesignsolutions.co.uk>
2022-05-22 19:28:10 +0600

    ok done

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,7 @@ export const environment = {
   api_url: 'https://raw.githubusercontent.com/thesefat/portfolio_api/main/',
   defaultTitle: 'Sefat Anam',
   navigationErrorTitle: 'An Error Occured',
-  navigationLoadingTitle: 'Loading...'
+  navigationLoadingTitle: 'Loading...',
+  unsplash_api:
+    'https://api.unsplash.com/users/im_sefat/photos?client_id=LDGIkb4OEgXMXLpMKWgXxvg8Qx18XinyoiUJYn2DC1g'
 };
