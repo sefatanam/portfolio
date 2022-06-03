14dcb62917859e781e5548e1bb53f22f413d15f6
thesefat <sefat@metadesignsolutions.co.uk>
2022-06-04 01:24:11 +0600

    project updated to ng version 14

--- src/app/art/art.component.ts
+++ src/app/art/art.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { Observable } from 'rxjs';
 import { Unimage } from '@interfaces/Unimage';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-art',
   templateUrl: './art.component.html',
-  styleUrls: ['./art.component.scss']
+  styleUrls: ['./art.component.scss'],
+  standalone: true,
+  imports: [CommonModule, HttpClientModule]
 })
 export class ArtComponent implements OnInit {
   images!: Observable<Unimage[]>;
