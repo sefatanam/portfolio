bf1b24668699e845960b4923bfaa8bb3c85ddc79
sefat.anam <msefat@saastaq.io>
2023-11-26 00:56:34 +0600

    feat(build)!: angular ssr implemented

---
+++ src/app/app.config.ts
@@ -0,0 +1,30 @@
+import { PreloadAllModules, RouterModule } from '@angular/router';
+import { RequestInterceptor } from '@interceptors/request.interceptor';
+import { routes } from './app-routing.module';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import {
+  provideClientHydration,
+  withHttpTransferCacheOptions
+} from '@angular/platform-browser';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    importProvidersFrom(
+      RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    ),
+    importProvidersFrom(HttpClientModule),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: RequestInterceptor,
+      multi: true
+    },
+    importProvidersFrom(CommonModule),
+    provideClientHydration(
+      withHttpTransferCacheOptions({
+        includePostRequests: true
+      })
+    )
+  ]
+};
